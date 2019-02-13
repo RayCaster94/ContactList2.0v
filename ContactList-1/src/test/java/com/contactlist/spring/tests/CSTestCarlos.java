859c4550b1edd7980b9241dfeba643d8029f2477
Carlos <carlos12194@gmail.com>
2019-02-13 14:40:35 +0100

    AÑADE PRUEBA UNITARIA (CARLOS)

--- ContactList-1/src/test/java/com/contactlist/spring/tests/CSTestCarlos.java
+++ ContactList-1/src/test/java/com/contactlist/spring/tests/CSTestCarlos.java
@@ -5,29 +5,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.context.embedded.LocalServerPort;
+//Para la Spring Boot 2.x
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.contactlist.spring.model.Contact;
 
-//ESTA PRUEBA COMPRUEBA SI HAY UN USUARIO EN LA BASE DE DATOS QUE SE APELLIDE "SANCHEZ"
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CSTestCarlos {
+	
+	public CSTestCarlos() {
+		
+	}
 
-	 @org.springframework.boot.web.server.LocalServerPort
+	Contact prueba;
+	
+	 @LocalServerPort
 	    private int port;
-	 
-	 @Autowired
-	 private TestRestTemplate restTemplate;
-	 
-	 @Test
-	 public void greetingShouldReturnDefaultMessage() throws Exception{
-		 
-		 assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/add",
-	                String.class).contains("lE"));
-		 
-	 }
-	 
+
+	    @Autowired
+	    private TestRestTemplate restTemplate;
+
+	    @Test
+	    public void greetingShouldReturnDefaultMessage() throws Exception {
+	        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+	                String.class)).contains("Ana");
+	    }
 }
